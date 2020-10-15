03fad22ea8231549abf8ca1897ac1b2c76a11d1b
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-10-15 19:11:52 -0300

    First commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,15 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack' }),
+);
 
 export default routes;
